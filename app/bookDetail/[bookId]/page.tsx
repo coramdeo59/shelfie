d62c901d47f605218b4d7d391918876cfbcabc70
coramdeo59 <melkamuelias59@gmail.com>
2025-01-01 22:25:29 +0300

    Implement useEffect hook for fetching book details

--- app/bookDetail/[bookId]/page.tsx
+++ app/bookDetail/[bookId]/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { supabase } from '@/lib/supabaseClient';
 import { useEffect, useState } from 'react';
 import { FaBookOpen, FaCheckCircle, FaArrowLeft } from 'react-icons/fa';
 import Image from 'next/image';
@@ -13,6 +14,26 @@ const BookDetail = ({ params }) => {
   const [message, setMessage] = useState('');
   const [summaryLength, setSummaryLength] = useState('short');
   const [showSummary, setShowSummary] = useState(false);
+
+  useEffect(() => {
+    const fetchBookDetails = async () => {
+      if (!bookId) return;
+
+      const { data, error } = await supabase
+        .from('reading_list')
+        .select('*')
+        .eq('book_id', bookId)
+        .single();
+
+      if (error) {
+        console.error('Error fetching book details:', error);
+      } else {
+        setBookDetails(data);
+      }
+    };
+
+    fetchBookDetails();
+  }, [bookId]);
 
   return (
     <div className="max-h-screen flex items-center font-mono justify-center p-6 w-full">
