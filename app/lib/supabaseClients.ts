d22499921e4b61eaa6d1de03dcb94f520e5a1188
coramdeo59 <melkamuelias59@gmail.com>
2025-01-04 20:55:26 +0300

    Fix circular import and update Supabase client initialization

--- app/lib/supabaseClients.ts
+++ app/lib/supabaseClients.ts
@@ -1,5 +1,5 @@
 
-import { createClient } from '@supabase/supabase-js';
+import { supabase } from '@/app/lib/supabaseClients';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
